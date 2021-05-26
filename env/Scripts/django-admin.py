271ee5fe28f92df62fbdeabf401fb226062ecb33
Mostafa Farahmand <mostafa.farahmand@ptwdosimetry.com>
2021-05-26 21:27:32 +0100

    edit19

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\denni\onedrive\desktop\djangopaypal_client\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
